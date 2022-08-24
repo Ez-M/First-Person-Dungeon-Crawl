ab41808cbacdcc3c560e83e786586b944a9388ac
Ezequiel <martinez.ezequiel42@gmail.com>
2022-08-23 19:52:24 -0700

    created C# class for player input scheme and added inputs to playerController

--- Assets/scripts/PlayerScripts/PlayerController.cs
+++ Assets/scripts/PlayerScripts/PlayerController.cs
@@ -1,9 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+
+    #region -Init Connections-
+
+    private PlayerManager playerManager;
+    private GameObject playerCap;
+    private Camera playerCam;
+
+    #endregion
+
+
+   private PlayerControls playerControls;
+
+   public InputAction moveForward;
+   public InputAction moveRight;
+   public InputAction moveBack;
+   public InputAction moveLeft;
+   public InputAction confirm;
+
+
+
+    private void Awake()
+    {
+        #region -INPUTS SET-
+        moveForward = playerControls.movement.moveforward;
+        moveRight = playerControls.movement.moveright;
+        moveBack = playerControls.movement.moveback;
+        moveLeft = playerControls.movement.moveleft;
+
+        confirm = playerControls.actions.confirmUse;
+
+        #endregion
+    }
+
     // Start is called before the first frame update
     void Start()
     {
