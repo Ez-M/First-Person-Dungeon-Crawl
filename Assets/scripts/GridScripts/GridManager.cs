d3c9dbc0610f7313c369c9a683375da173c29926
Ezequiel <martinez.ezequiel42@gmail.com>
2022-08-30 02:52:40 -0700

    Created tile and gridmanager scripts, began working on basic grid generation

---
+++ Assets/scripts/GridScripts/GridManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int width, height;
+
+    [SerializeField] private TileGen tile;
+
+    void GenerateGrid() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++){
+                
+            }
+        }
+    }
+
+}
